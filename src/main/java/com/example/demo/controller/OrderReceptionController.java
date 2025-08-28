fb9f520f6cf888993619b12da2cdf580e20c94c3
Ahmed Yasser <ay622004@gmail.com>
2025-08-28 20:32:39 +0300

    Task 5 working with autherauthorization

--- src/main/java/com/example/demo/controller/OrderReceptionController.java
+++ src/main/java/com/example/demo/controller/OrderReceptionController.java
@@ -6,6 +6,7 @@ import com.example.demo.service.OrderReceptionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -16,6 +17,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/admin/order/history")
+@PreAuthorize("hasRole('ADMIN')")
 public class OrderReceptionController {
 
     @Autowired
